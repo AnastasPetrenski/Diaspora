41c706cd0a16062710d8d99cb246d0411beef5a2
AnastasPetrenski <hloro83@gmail.com>
2021-04-11 10:45:46 +0300

    AddTest

---
+++ Diaspora.Web/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diaspora.Web.Controllers
+{
+    public class TestController
+    {
+        public int Id { get; set; }
+    }
+}
